bd31bc4af93bea402052f12f57f1ad2bc8d0cf95
aadityavaze <aadityavikasvaze@gmail.com>
2021-10-25 14:27:41 +0530

    bugs fixed and servo removed

--- sitbit.ino
+++ sitbit.ino
@@ -3,11 +3,10 @@
 #include <Arduino_LPS22HB.h>
 #include <Arduino_LSM9DS1.h>
 #include <PDM.h>
-#include <Servo.h>
+#include <climits> 
 
 
 #define HEART_LIGHT 8   
-#define HEART_CLOCK 7  
 
 #define PERSON_SITTING 0
 #define PERSON_EXERCISING 1
@@ -18,8 +17,6 @@
 #define IDEAL_NO_OF_UPDATES_TO_EXERCISE 180
 
 
-Servo servo1;
-
 int userState = PERSON_IDLE;
 
 
@@ -27,8 +24,6 @@ char buff[100];
 int chairClock = 0;
 float heartIntensity = -1.0;
 int updateIntensity = 1;
-int idealChairTime = (1000*60)*0.1;
-//int updateRate1 = idealChairTime/180;
 int updateRate1 = 0;
 int i = 0;
 long lastUpdate1;
@@ -42,12 +37,8 @@ int rotationVal = 0;
 
 void setup() {
     pinMode(HEART_LIGHT, OUTPUT);
-    servo1.attach(HEART_CLOCK);
-    //reset heart clock
-    servo1.write(0);
     
     Serial.begin(115200);
-    while (!Serial) {}
     
     if (!APDS.begin()) {
         Serial.print("APDS init failed");
@@ -55,27 +46,14 @@ void setup() {
     }
 }
 
-
-void resetClock(){
-  chairClock = 0;
-  rotationVal = 0;  
-  servo1.write(rotationVal);
-  //Serial.println(chairClock);
-}
-
-void updateClock(){
-  chairClock++;
-  rotationVal+=1;
-  servo1.write(rotationVal);
-  //Serial.println(rotationVal);
-  
-}
 void loop() {
 
 
   switch(userState){
-    case PERSON_IDLE: updateRate2 = 500;
-    updateRate1 = 100000;
+    case PERSON_IDLE: 
+    updateRate2 = INT_MAX;
+    updateRate1 = INT_MAX;
+   analogWrite(8, 255);
     break;
     case PERSON_SITTING: updateRate2 = 50;
     updateRate1 = 50;
@@ -86,11 +64,11 @@ void loop() {
     break;
 
     case PERSON_CHEATING: updateRate2 = 1;
-    updateRate1 = 100000;
+    updateRate1 = INT_MAX;
     break;
 
     case REMIND_PERSON_TO_STAND: updateRate2 = 1;
-    updateRate1 = 100000;
+    updateRate1 = INT_MAX;
     break;
     }
 
@@ -102,7 +80,6 @@ void loop() {
           }
      if(userState == PERSON_EXERCISING){
         if(totalUpdates1> IDEAL_NO_OF_UPDATES_TO_EXERCISE){
-                resetClock();
                 userState = PERSON_IDLE;
                 totalUpdates1 = 0;
                 }
@@ -120,34 +97,28 @@ void loop() {
      if (proximityAvailable) {
 
        if(proximity <= 220 && userState == PERSON_IDLE){
-           resetClock();
            userState = PERSON_SITTING;
           }
           
 
        if(proximity > 220 && userState == PERSON_SITTING){
-          resetClock();
           userState = PERSON_IDLE;
         }
       
 
        if(proximity > 220 && userState == REMIND_PERSON_TO_STAND){
-           resetClock();
            userState = PERSON_EXERCISING;
            totalUpdates1 = 0;
           }
 
         if(proximity > 220 && userState == PERSON_CHEATING){
-          resetClock();
            userState = PERSON_EXERCISING;
                   }
-          //       if(proximity > 220 && userState!= PERSON_EXERCISING){
-//        userState = PERSON_IDLE;
-//        }
 
           if(millis() - lastUpdate1>=updateRate1)
         {
             lastUpdate1 = millis();
+            Serial.println(userState);
             
                if(proximity > 220){
                 if(userState == PERSON_SITTING){
@@ -158,7 +129,6 @@ void loop() {
                
                 if(userState == PERSON_EXERCISING){
                   totalUpdates1++;
-                  updateClock();
                   }
                
                         
@@ -166,16 +136,13 @@ void loop() {
             else{
               if(userState == PERSON_SITTING){
                  totalUpdates1++;
-                 updateClock();
                 }
               if(userState == PERSON_EXERCISING){
-                Serial.println("Cheating");
                 totalUpdates1 = 0;
                 userState = PERSON_CHEATING;
                 
                 }
               else if(userState != PERSON_SITTING){
-                resetClock();
                 userState = PERSON_SITTING;
                 }
                
@@ -189,9 +156,8 @@ void loop() {
 
       if(millis() - lastUpdate2>=updateRate2){
         
-        Serial.println(userState);
+
         lastUpdate2 = millis();
-        //Serial.println(map(100*(1.0 + sin(heartIntensity)),0,200,0,255));
         analogWrite(8,map(100*(1.0 + sin(heartIntensity)),0,200,0,255));
         heartIntensity = heartIntensity + 0.05;
 
